484765c94a046674520ea79c6b3da565b2b8b7c2
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2026-04-17 01:17:06 +0530

    Added some typescript code which is similar in rust

---
+++ Web3/Rust for Solana/Rust-1/main.ts
@@ -0,0 +1,18 @@
+// Equivalent Code in TypeScript
+
+function main()
+{
+    let x: number = 25;
+    let y: string = "hello";
+    console.log(x);
+    console.log(y);
+}
+main();
+
+// functions in ts
+function doSum(a: number, b:number){
+    return a+b;
+}
+
+let ans = doSum(10,12);
+console.log(ans);
