cdea0f56fb16edde8d3e64c21cf82185154b0aac
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2026-05-22 13:51:35 +0530

    Added code for modern imports & exports in typescript

---
+++ WebDev + DevOps/Typescript/Generics, enums, Imports_exports/imports_exports.ts
@@ -0,0 +1,24 @@
+
+// **** Importing modules
+
+// old way of imports & exports
+// const express = require("express");
+
+// new way
+// import express from "express";
+
+
+// exporting modules
+
+// / new way
+export const a = 10;
+
+// module.exports = {  // old way
+//     a:1
+// }
+
+
+// ***** default express
+
+const num = 19;
+export default num;
