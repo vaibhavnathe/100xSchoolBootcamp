0b51a58bf32a69f65e0bb86bed77afce4f063954
Vaibhav Nathe <vaibhavnathe2002@gmail.com>
2026-06-02 02:05:00 +0530

    Added basic functionality App component which renders diff posts

--- WebDev + DevOps/React/React-1/todo-app/src/App.tsx
+++ WebDev + DevOps/React/React-1/todo-app/src/App.tsx
@@ -1,122 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
+
+// tsx/jsx can returned this html, js code , but ts can't return html code
+
+// component :- building blocks of UI. They allow you to split UI into independent, reusable piece which is component
+// props :- In React, props (short for "properties") are read-only objects used to pass data from a parent component to a child component
+// useState :- h\Hook that lets you add state to functional component. It return an array with current state and function to update it.
 function App() {
-  const [count, setCount] = useState(0)
+
+   // write valid ts
+   // we will get some data from backend like this json format
+  //  const posts = await axios.get("/posts/something")
+   const posts = [{
+      name: "Hakirat", 
+      content : "Hi this is my new Course for WebDev + DevOps"
+    },{
+      name: "Raman", 
+      content : "Hi I started learning ML"
+    },
+    {
+      name: "Shivay", 
+      content : "Hi I have enrolled in the cohort 3"
+    },
+    {
+      name: "Sachin", 
+      content : "Hi I got a job"
+    }
+   ];
+
+  //  const postComponents = posts.map(post => <Post name={post.name} content={post.content}/>);
+
+  //  const postComponents = [
+  //       <Post name={posts[0].name} content={posts[0].content}/>,
+  //       <Post name={posts[1].name} content={posts[1].content}/>
+  //  ]
+
+
+  // Question :- what will/should happen for below code -> First all above posts array get rendered on page, 
+  //            & then this setInterval will run for each sec & will add one post(name, content) object 
+   //           but the posts will not re-rendered on page coz it only getting pushed to posts array not rendering on page.
+   //           Thats why we use useState hook
+  setInterval(() => {
+    console.log("Set Interval");
+    posts.push({
+      name: "Kirat",
+      content: "Kirat is my name & coding is my game"
+    });
+    console.log("Posts: " , posts);
+  }, 1000);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1>LinkedIn!!!</h1>
 
-      <div className="ticks"></div>
+      {/* Redenring backend post data */}
+      {posts.map(post => <Post name={post.name} content={post.content}/>)}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      {/* OR */}
+      
+      {/* {postComponents} */}
+      
+      {/* OR */}
+      
+      {/* <Post name={posts[0].name} content={posts[0].content}/>
+      <Post name={posts[1].name} content={posts[1].content}/> */}
+      
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      {/* // hardcoded post data */}
+      {/* <Post name="Harkirat" content="Hi this is my new Course for WebDev + DevOps"/>
+      <Post name="Raman" content="Hi I started learning ML"/>
+      <Post name="Shreya" content="Hi I enrolled in cohort 3"/>
+      <Post name="Shivam" content="Hi I got a job today"/>
+      <Post name="Sachin" content="Hi I got a internship"/> */}
+    </div>
   )
 }
 
-export default App
+export default App;
+
+// post component for linkedin
+function Post(props)
+{
+  return (
+    <div style={{backgroundColor: "lightblue",margin: "10px", padding: "10px", borderRadius: "4px", fontSize: 20, border: "2px solid black"}}>
+
+      <div>
+        <b>{props.name}</b>
+      </div>
+
+      <div>
+        {props.content}
+      </div>
+
+    </div>
+  )
+}
